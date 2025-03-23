3a3626adae0ef7faf2158300e63fe801b4b9ce07
SayVathanak <saksovathanaksay@gmail.com>
2025-03-23 12:46:46 +0700

    Done

--- models/Order.js
+++ models/Order.js
@@ -4,12 +4,12 @@ const orderSchema = new mongoose.Schema({
     userId: { type: String, required: true, ref: 'user' },
     items: [{
         product: { type: String, required: true, ref: 'product' },
-        quantiy: { type: Number, required: true }
+        quantity: { type: Number, required: true }
     }],
     amount: { type: Number, required: true },
     address: { type: String, ref: 'address', required: true },
     status: { type: String, required: true, default: 'Order Placed' },
-    data: { type: Number, required: true }
+    date: { type: Number, required: true }
 })
 
 const Order = mongoose.models.order || mongoose.model('order', orderSchema)
